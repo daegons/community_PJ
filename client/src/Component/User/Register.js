37238e134d854123391c5d2d8f36cfb3a7f9c50c
daegons <qzom1425@naver.com>
2023-05-14 19:16:42 +0900

    firebase인증 작업

--- client/src/Component/User/Register.js
+++ client/src/Component/User/Register.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { LoginDiv } from '../../Style/UserCSS';
 
+import firbase from '../../firebase';
+
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCF, setPasswordCF] = useState('');
+
+  const registerFunc = (e) => {
+    e.preventDefault();
+    if (!(name && email && password && passwordCF)) {
+      return alert('빠진 부분이 없나 확인해주세요.');
+    }
+    if (password !== passwordCF) {
+      return alert('비밀번호 확인 불일치');
+    }
+    //파이어 베이스 인증시간이 걸려서.. 위에 async await 걸어줌
+    const createdUser = firbase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+  };
   return (
     <LoginDiv>
       <form action="">
@@ -41,7 +57,7 @@ const Register = () => {
             setPasswordCF(e.currentTarget.value);
           }}
         />
-        <button>가입신청</button>
+        <button onClick={registerFunc}>가입신청</button>
       </form>
     </LoginDiv>
   );
