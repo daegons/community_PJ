c0a2e5beb30ac36529c7714e54cadb443fa61e74
daegons <qzom1425@naver.com>
2023-05-15 23:44:58 +0900

    nameCheck.feat

--- client/src/Component/User/Register.js
+++ client/src/Component/User/Register.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { LoginDiv } from "../../Style/UserCSS";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LoginDiv } from '../../Style/UserCSS';
 
-import firebase from "../../firebase";
+import firebase from '../../firebase';
 
-import axios from "axios";
+import axios from 'axios';
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordCF, setPasswordCF] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCF, setPasswordCF] = useState('');
   const [loaded, setLoaded] = useState(false);
+  const [nameCheck, setNameCheck] = useState(false);
+  const [nameInfo, setNameInfo] = useState('');
 
   const navigate = useNavigate();
 
@@ -19,10 +21,14 @@ const Register = () => {
     setLoaded(true);
     e.preventDefault();
     if (!(name && email && password && passwordCF)) {
-      return alert("빠진 부분이 없나 확인해주세요.");
+      return alert('빠진 부분이 없나 확인해주세요.');
     }
+
     if (password !== passwordCF) {
-      return alert("비밀번호 확인 불일치");
+      return alert('비밀번호 확인 불일치');
+    }
+    if (!nameCheck) {
+      return alert('닉네임 중복검사를 진행해 주세요.');
     }
     //파이어 베이스 인증시간이 걸려서.. 위에 promise로 async await 걸어줌
     const createdUser = await firebase
@@ -38,28 +44,79 @@ const Register = () => {
       email: createdUser.user.multiFactor.user.email,
       uid: createdUser.user.multiFactor.user.uid,
     };
-    axios.post("/api/user/register", body).then((res) => {
+    axios.post('/api/user/register', body).then((res) => {
       setLoaded(false);
       if (res.data.success) {
         //회원가입 성공시
-        navigate("/login");
+        navigate('/login');
       } else {
         //회원가입 실패시
-        return alert("회원가입 실패했어요..");
+        return alert('회원가입 실패했어요..');
+      }
+    });
+  };
+
+  const nameCheckHandler = (e) => {
+    e.preventDefault();
+    if (name === null) {
+      return alert('닉네임을 입력해주세요');
+    }
+    let body = {
+      displayName: name,
+    };
+    axios.post('/api/user/namecheck', body).then((res) => {
+      if (res.data.success) {
+        if (res.data.check) {
+          setNameCheck(true);
+          setNameInfo(
+            <div
+              style={{
+                fontWeight: 'bold',
+                fontFamily: 'Yeon Sung',
+              }}
+            >
+              사용<span style={{ color: 'green' }}> 가능한 </span>
+              닉네임입니다.
+            </div>
+          );
+        } else {
+          setNameInfo(
+            <div
+              style={{
+                fontWeight: 'bold',
+                fontFamily: 'Yeon Sung',
+              }}
+            >
+              사용
+              <span
+                style={{
+                  color: 'red',
+                }}
+              >
+                {' '}
+                불가한{' '}
+              </span>
+              닉네임입니다.
+            </div>
+          );
+        }
       }
     });
   };
   return (
     <LoginDiv>
       <form action="">
-        <label htmlFor="">이름</label>
+        <label htmlFor="">닉네임</label>
         <input
           type="name"
           value={name}
+          disabled={nameCheck} //nameCheck가 true로 되면 input값 고정
           onChange={(e) => {
             setName(e.currentTarget.value);
           }}
         />
+        {nameInfo}
+        <button onClick={nameCheckHandler}>닉네임 중복검사</button>
         <label htmlFor="">이메일</label>
         <input
           type="email"
